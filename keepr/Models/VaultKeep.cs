ed72b9933ab0a8e5b14a45977aa3bc089d3268f6
Andrew Allison <andrewallison5211@gmail.com>
2023-08-25 13:59:53 -0600

    vk model

---
+++ keepr/Models/VaultKeep.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace keepr.Models;
+
+public class VaultKeep : RepoItem<int>
+{
+    public string CreatorId { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+
+}
