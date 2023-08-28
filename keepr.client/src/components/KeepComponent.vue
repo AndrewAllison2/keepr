92c5fcaf3934851557f375cff4ddf204d00dde0f
Andrew Allison <andrewallison5211@gmail.com>
2023-08-28 14:11:57 -0600

    cards styled

--- keepr.client/src/components/KeepComponent.vue
+++ keepr.client/src/components/KeepComponent.vue
@@ -1,12 +1,22 @@
 <template>
-
   
-        <img class="img-fluid keep-img selectable" data-bs-toggle="modal" data-bs-target="#keepModal" @click="setActiveKeep(this.keepProp)" :src="keepProp.img" :alt="keepProp.name">
+  <div class="cards">
+    <img class="img-fluid keep-img selectable" data-bs-toggle="modal" data-bs-target="#keepModal" @click="setActiveKeep(this.keepProp)" :src="keepProp.img" :alt="keepProp.name">
+    
+  </div>
+  
+  <div class="keep-info d-flex justify-content-around align-items-center">
+    <h5>{{ keepProp?.name }}</h5>
+    <img class="img-fluid avatar" :src="keepProp?.creator?.picture" :alt="keepProp?.creator?.name">
+  </div>
 
 
 
 
 </template>
+
+
+
 
 
 <script>
@@ -39,5 +49,23 @@ export default {
 .keep-img{
 border-radius: 3%;
 object-fit: contain;
+}
+
+div{
+  max-height:min-content;
+}
+
+.keep-info{
+  position: relative;
+  top: -3.2em;
+  left: -0em;
+  margin-bottom: -3em;
+  text-shadow: 2px 2px black;
+}
+
+.avatar{
+  height: 3em;
+  width: 3em;
+  border-radius: 50%;
 }
 </style>
