06be1bedb0ccc7ed86fcf3a54f3e96d1b2d3273f
Andrew Allison <andrewallison5211@gmail.com>
2023-10-25 17:57:08 -0600

    new db

--- keepr/dbSetup.sql
+++ keepr/dbSetup.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE my_db
+CREATE DATABASE keepr
     DEFAULT CHARACTER SET = 'utf8mb4';
 
-USE my_db;
+USE keepr;
 
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
